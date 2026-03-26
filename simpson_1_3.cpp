eb160763d468dccae037eff7e84aa726bf589a49
souvik-collab <souvik.learn@gmail.com>
2026-03-26 21:13:34 +0530

    Add files via upload

---
+++ simpson_1_3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+float f(float x) {
+    return  ( x * x);
+}
+
+// Trapezoidal Rule implementation
+float simp13(float a, float b, int n) {
+    float h = (b - a) / n; // Width of each sub-interval
+    float sum = f(a) + f(b); // Sum of first and last term
+
+    // Sum of middle terms multiplied by 2
+    for (int i = 1; i < n; i++) {
+        if (i%2==0)
+        {
+            sum += 2 * f(a + i * h);
+        }
+        else
+        {
+            sum += 4 * f(a + i * h);
+        }
+    }
+
+    return (h / 3) * sum;
+}
+
+int main() {
+
+    float lower_limit , upper_limit ;
+    int intervals; 
+    cout<<"Enter Lower Limit: ";
+    cin>>lower_limit;
+    cout<<"Enter Upper Limit: ";
+    cin>>upper_limit;
+    cout<<"Enter No. of Intervals: ";
+    cin>>intervals;
+    float result = simp13(lower_limit, upper_limit, intervals);
+    cout << "The integral value is: " << result << std::endl;
+
+    return 0;
+}
